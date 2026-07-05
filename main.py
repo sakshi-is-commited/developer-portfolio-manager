3ed65f9491a996ed9167ad49fad58483dce55792
sakshi-is-commited <shakshiadarkar10@gmail.com>
2026-07-05 12:35:25 +0530

    feat: implement CLI menu

---
+++ main.py
@@ -0,0 +1,16 @@
+from src.menu import display_menu
+
+def main():
+    while True:
+        display_menu()
+
+        choice = input("\nEnter your choice: ")
+
+        if choice == "6":
+            print("\nGoodbyee👋🏻")
+
+        else:
+            print("\nFeature not implemented yet.")
+
+if __name__ == "__main__":
+    main()
