38ee782a1cf0a4815075cc5b1af894b8ec3a8f6c
sakshi-is-commited <shakshiadarkar10@gmail.com>
2026-07-11 15:59:07 +0530

    refactor: moved menu.py to cli

---
+++ src/cli/menu.py
@@ -0,0 +1,11 @@
+def display_menu():
+    print("\n ===========================")
+    print("Developer Portfolio Manager")
+    print("==============================")
+    print("1. Add Project")
+    print("2. View Project")
+    print("3. Search Project")
+    print("4. Update Project")
+    print("5. Delete Project")
+    print("6. Exit")
+    
